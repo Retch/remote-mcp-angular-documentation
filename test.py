2086fb3ce91d8d9a8aa7d9cb2c35677a4b0c1515
Retch <retch100@gmail.com>
2026-03-21 14:52:19 +0100

    add test code to call sample mcp server

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+from fastmcp import Client
+import asyncio
 import modules.retrieval as Retriever
 
-retrieved_docs = Retriever.find_docs("ngClass")
+#retrieved_docs = Retriever.find_docs("ngClass")
 
-print(len(retrieved_docs))
+#print(len(retrieved_docs))
+
+
+client = Client("http://localhost:8000/mcp")
+
+
+async def call_tool(name: str):
+    async with client:
+        result = await client.call_tool("greet", {"name": name})
+        print(result)
+
+asyncio.run(call_tool("Ford"))
