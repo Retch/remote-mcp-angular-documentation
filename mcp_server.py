52639fa0a6b0b143bdd9a11cad7a12007db6369d
Retch <retch100@gmail.com>
2026-03-21 16:10:11 +0100

    add mcp tool search_docs

--- mcp_server.py
+++ mcp_server.py
@@ -1,10 +1,13 @@
 from fastmcp import FastMCP
+import modules.retrieval as Retriever
+from type_definitions.retrieved_doc import RetrievedDoc
 
 mcp = FastMCP("Angular Documentation MCP Server")
 
 @mcp.tool
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+def search_docs(query: str) -> list[str]:
+    retrieved_docs: list[RetrievedDoc] = Retriever.find_docs("ngClass")
+    return [t.text for t in retrieved_docs]
 
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
